ab8e9fca6df058fe191971a43a7b3ef529d4ec2c
cuiyi <cuiyi@bytedance.com>
2018-09-23 10:07:32 +0800

    Use irony

--- setup-elpa.el
+++ setup-elpa.el
@@ -1,9 +1,8 @@
-
 ;; irony configure
-;; (add-hook 'c++-mode-hook 'irony-mode)
-;; (add-hook 'c-mode-hook 'irony-mode)
-;; (add-hook 'objc-mode-hook 'irony-mode)
-;; 
-;; (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
+(add-hook 'c++-mode-hook 'irony-mode)
+(add-hook 'c-mode-hook 'irony-mode)
+(add-hook 'objc-mode-hook 'irony-mode)
+
+(add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
 ;; =================================
 
--- setup-lisp.el
+++ setup-lisp.el
@@ -1,6 +1,5 @@
 (require 'projectile)
 (require 'setup-ggtags)
-(require 'setup-irony)
 
 
 (projectile-mode)
@@ -8,31 +7,31 @@
   '(add-hook 'flycheck-mode-hook #'flycheck-irony-setup))
 
 ;; rtags configure
-(require 'rtags)
-(require 'company-rtags)
-(require 'flycheck-rtags)
-(setq rtags-completions-enabled t)
-(setq rtags-autostart-diagnostics t)
-
-(add-hook 'c-mode-hook 'rtags-start-process-unless-running)
-(add-hook 'c++-mode-hook 'rtags-start-process-unless-running)
-(add-hook 'objc-mode-hook 'rtags-start-process-unless-running)
-
-
-(eval-after-load 'company
-  '(add-to-list
-    'company-backends 'company-rtags))
-
-(rtags-enable-standard-keybindings)
-
-
-(defun my-flycheck-rtags-setup ()
-    (flycheck-select-checker 'rtags)
-	  (setq-local flycheck-highlighting-mode nil) ;; RTags creates more accurate overlays.
-	    (setq-local flycheck-check-syntax-automatically nil))
-(add-hook 'c-mode-hook 'my-flycheck-rtags-setup)
-(add-hook 'c++-mode-hook 'my-flycheck-rtags-setup)
-(add-hook 'objc-mode-hook 'my-flycheck-rtags-setup)
+;; (require 'rtags)
+;; (require 'company-rtags)
+;; (require 'flycheck-rtags)
+;; (setq rtags-completions-enabled t)
+;; (setq rtags-autostart-diagnostics t)
+;; 
+;; (add-hook 'c-mode-hook 'rtags-start-process-unless-running)
+;; (add-hook 'c++-mode-hook 'rtags-start-process-unless-running)
+;; (add-hook 'objc-mode-hook 'rtags-start-process-unless-running)
+;; 
+;; 
+;; (eval-after-load 'company
+;;   '(add-to-list
+;;     'company-backends 'company-rtags))
+;; 
+;; (rtags-enable-standard-keybindings)
+;; 
+;; 
+;; (defun my-flycheck-rtags-setup ()
+;;     (flycheck-select-checker 'rtags)
+;; 	  (setq-local flycheck-highlighting-mode nil) ;; RTags creates more accurate overlays.
+;; 	    (setq-local flycheck-check-syntax-automatically nil))
+;; (add-hook 'c-mode-hook 'my-flycheck-rtags-setup)
+;; (add-hook 'c++-mode-hook 'my-flycheck-rtags-setup)
+;; (add-hook 'objc-mode-hook 'my-flycheck-rtags-setup)
 
 ;; =================================
 ;; irony
